ca2ad8e4d4b194739d38cd7315422f3ef8d20b37
ITMX\wassana.kim <wassana.kimonit@gmail.com>
2023-11-27 14:50:28 +0700

    Edit Product page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import Product from './components/Product/Product';
-import Footer from './components/Footer/Footer';
+import Product from './component/Product/Product';
+import Footer from './component/Footer/Footer';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
-import Banner from './components/Banner/Banner';
-import NavBar from './components/NavBar/Navbar';
+import Banner from './component/Banner/Banner';
+import NavBar from './component/NavBar/Navbar';
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;1,100;1,200;1,400&family=Lora:ital,wght@0,400;0,600;1,500&family=Playfair+Display&display=swap');
 </style>
